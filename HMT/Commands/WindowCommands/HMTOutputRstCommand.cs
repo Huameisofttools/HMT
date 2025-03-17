dbdf388247bd7c966f271ee8902badfa1aa7a8d0
Willie Yao <willie.yao@huameisoft.cn>
2025-03-17 07:34:26 +0000

    Commit by Willie Yao - New output window - 03/17/2025

---
+++ HMT/Commands/WindowCommands/HMTOutputRstCommand.cs
@@ -0,0 +1,56 @@
+﻿using HMT.Views.Global;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Threading.Tasks;
+
+namespace HMT.Commands.WindowCommands
+{
+    internal sealed class HMTOutputRstCommand
+    {
+        public const int CommandId = 0x0145;
+
+        public static readonly Guid CommandSet = new Guid("194ef7a6-070b-47e5-b084-193c13aa350a");
+
+        private readonly AsyncPackage package;
+
+        private HMTOutputRstCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        public static HMTOutputRstCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new HMTOutputRstCommand(package, commandService);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Get the instance number 0 of this tool window. This window is single instance so this instance
+            // is actually the only one.
+            // The last flag is set to true so that if the tool window does not exists it will be created.
+            ToolWindowPane window = this.package.FindToolWindow(typeof(HMTOutputRstWindow), 0, true);
+            if ((null == window) || (null == window.Frame))
+            {
+                throw new NotSupportedException("Cannot create tool window");
+            }
+
+            IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+    }
+}
---
+++ HMT/Views/Global/HMTOutputRst.xaml.cs
@@ -0,0 +1,64 @@
+﻿using HMT.Copilot;
+using HMT.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using suiren.Models;
+using suiren.Services;
+using suiren.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HMT.Views.Global
+{
+    public class HMTOutputRstViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<HMTOutputRstMessage> _messages = new ObservableCollection<HMTOutputRstMessage>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<HMTOutputRstMessage> Messages
+        {
+            get => _messages;
+            set => SetField(ref _messages, value);
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for HMTOutputRst.xaml
+    /// </summary>
+    public partial class HMTOutputRst : UserControl
+    {
+        public HMTOutputRst()
+        {
+            InitializeComponent();
+        }
+    }
+}
